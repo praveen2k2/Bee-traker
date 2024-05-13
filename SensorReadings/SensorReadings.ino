354d6f4b45b416ede80e190224f0193a9357f5f3
Naveen Sanjaya <naveensanjayab@gmail.com>
2024-05-13 07:31:58 +0530

    feat: arduino SensorReadings file feat: temperature reading code

---
+++ SensorReadings/SensorReadings.ino
@@ -0,0 +1,55 @@
+#include "DHT.h"
+#define DHTPIN 2  // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float h, t, f, hic, hif;
+  // Call the temp_humid function and assign the return values to variables
+  temp_humid(h, t, f, hic, hif);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  // Add a delay or other logic here if needed
+}
+
+void temp_humid(float &h, float &t, float &f, float &hic, float &hif) {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    // Set values to NaN to indicate failure
+    h = t = f = hic = hif = NAN;
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  hic = dht.computeHeatIndex(t, h, false);
+}
