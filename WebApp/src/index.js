399a120746e924e7d1b868b29a9850651632a3d6
Naveen Sanjaya <naveensanjayab@gmail.com>
2024-05-22 06:52:36 +0530

    feat: read data from firebase

--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -1,21 +1,18 @@
-console.log("hello");
-
 // Import the functions need from the SDKs
-import { initializeApp } from "/firebase/app";
-import { getDatabase } from "/firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyA6RyU5sX58C9uhyN1QYAvbMZhn8m3eP3Y",
-    authDomain: "hivelink-abd1a.firebaseapp.com",
-    databaseURL: "https://hivelink-abd1a-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "hivelink-abd1a",
-    storageBucket: "hivelink-abd1a.appspot.com",
-    messagingSenderId: "843058360587",
-    appId: "1:843058360587:web:d1d90e47e657e2bb53320e",
-    measurementId: "G-B31PD72N8G",
-    databaseURL: "https://hivelink-abd1a-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: "AIzaSyA6RyU5sX58C9uhyN1QYAvbMZhn8m3eP3Y",
+  authDomain: "hivelink-abd1a.firebaseapp.com",
+  databaseURL: "https://hivelink-abd1a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hivelink-abd1a",
+  storageBucket: "hivelink-abd1a.appspot.com",
+  messagingSenderId: "843058360587",
+  appId: "1:843058360587:web:d1d90e47e657e2bb53320e",
+  measurementId: "G-B31PD72N8G"
 };
 
 // Initialize Firebase
