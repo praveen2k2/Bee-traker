6949ed7ce8885294fcfb03307f81570fc678c3a8
Naveen Sanjaya <naveensanjayab@gmail.com>
2024-05-22 06:15:00 +0530

    modified

--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -1,11 +1,12 @@
-// Import the functions need from the SDKs
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+console.log("hello");
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp({
+// Import the functions need from the SDKs
+import { initializeApp } from "/firebase/app";
+import { getDatabase } from "/firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
     apiKey: "AIzaSyA6RyU5sX58C9uhyN1QYAvbMZhn8m3eP3Y",
     authDomain: "hivelink-abd1a.firebaseapp.com",
     databaseURL: "https://hivelink-abd1a-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -13,8 +14,32 @@ const firebaseApp = initializeApp({
     storageBucket: "hivelink-abd1a.appspot.com",
     messagingSenderId: "843058360587",
     appId: "1:843058360587:web:d1d90e47e657e2bb53320e",
-    measurementId: "G-B31PD72N8G"
+    measurementId: "G-B31PD72N8G",
+    databaseURL: "https://hivelink-abd1a-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+// Replace 'a' with the actual user ID you want to fetch data for
+const userRef = ref(database, 'Users/a');
+
+onValue(userRef, (snapshot) => {
+  const userData = snapshot.val();
+  
+  if (userData) {
+    const temperature = userData.Temperature;
+    const humidity = userData.Humidity;
+    
+    // Process the temperature and humidity values here
+    console.log("Temperature:", temperature, "°C");
+    console.log("Humidity:", humidity, "%");
+  } else {
+    console.log("No sensor data available for this user");
+  }
 });
 
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
