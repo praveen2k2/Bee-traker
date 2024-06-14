ff95e0d3b74e92c826d341465fb71a2a7d0ff372
Praveen Himasha Wijesinghe <himasha2k2@gmail.com>
2024-06-14 07:59:30 +0530

    Update main.ino

--- Main Code/ATMEGE328/main/main.ino
+++ Main Code/ATMEGE328/main/main.ino
@@ -1,32 +1,57 @@
-#define dht11
-#define ir A0
-#define loadcell1
-#define loadcell2
-#define irPower
-#define dhtPower
-#define esppower
+#define ir 14 
+#define loadcell1 2
+#define loadcell2 3
+#define dhtPower 4
+#define loadCSwitch 5
+#define espPower 6
+#define irPower 7
 
 int beeCount=0;
+bool currentState;
 
 void setup() {
-  
+  pinMode(ir, INPUT);
+  pinMode(loadcell1, INPUT);
+  pinMode(loadcell2, INPUT);
+  pinMode(dhtPower, OUTPUT);
+  pinMode(loadCSwitch, OUTPUT);
+  pinMode(espPower, OUTPUT);
+  pinMode(irPower, OUTPUT);
 
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Starting HiveLink....");
+  currentState= digitalRead(ir);
+  Serial.print(currentState);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  beeCount=updateBeeCount(beeCount);
+  //Serial.println(beeCount);
+  delay(10);//a delay is need to get readings correctly
 
 }
 
-//get time from esp32
-int gettime(){
-  Serial.begin(9600);
-  Serial.write('gt');
-  int time=Serial.read();
-  return time;
-}
 
 //get bee count 
 int updateBeeCount(int beeCount){
-   return beeCount++;
+  bool newState=digitalRead(ir);
+  if(currentState!=newState){
+    beeCount ++;
+    currentState=newState;
+     //upadate here to increase the time 
+    if(beeCount > 10){
+    //Serial.begin(9600);
+    //while (!Serial) {;}
+    Serial.print(1);
+    beeCount=0;
+    delay(1000);
+   }
+   }
+  return beeCount;
 }
+
+
